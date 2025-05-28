86efca70a2777d2fe774eac2a7cfa17ca1c831ff
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-28 17:18:00 +0200

    fix: replace anchor tag with NavLink for the logo in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
                 <div className="container-fluid space ">
-                    <a className="navbar-brand" href="#"><img src={logo} className="rounded-circle" style={{ width: "100px" }} alt="logo" /></a>
+                    <NavLink to="/" className="navbar-brand" href="#"><img src={logo} className="rounded-circle" style={{ width: "100px" }} alt="logo" /></NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
