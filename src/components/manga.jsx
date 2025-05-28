ab3476940e65c63513e6e7866ff7c868d6599d9d
alemalagni <alemalagni@gmail.com>
2025-05-28 11:31:19 +0200

    feat: add manga function

--- src/components/manga.jsx
+++ src/components/manga.jsx
@@ -1,5 +1,16 @@
-function manga() {
-
+function manga({ img, title, price }) {
+    return (
+        <div>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <strong>{title}</strong>
+                <p>{price}</p>
+            </div>
+            <button>Acquista</button>
+        </div>
+    )
 }
 
 export default manga;
