1ce7d860e49505fb9bc0189e7cc167ae90c9a7de
EstebanCaponio <estebancaponio@gmail.com>
2025-05-28 18:56:00 +0200

    feat: fix

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -36,22 +36,22 @@ import { Link } from "react-router-dom"
 function SeriesCard({ serie }) {
     return (
         <div className="card border-0 shadow-lg rounded-4 h-100 bg-white position-relative overflow-hidden anime-card">
-<img
-    className="card-img-top object-fit-contain rounded-top-4"
-    src={serie.imagePath}
-    alt={serie.name}
-    style={{
-        height: "260px",
-        objectFit: "contain",
-        backgroundColor: "#f8f8f8", // per riempire eventuali spazi vuoti
-    }}
-/>
+            <img
+                className="card-img-top object-fit-contain rounded-top-4"
+                src={serie.imagePath}
+                alt={serie.name}
+                style={{
+                    height: "260px",
+                    objectFit: "contain",
+                    backgroundColor: "#f8f8f8", // per riempire eventuali spazi vuoti
+                }}
+            />
 
             <div className="card-body d-flex flex-column">
                 <h5 className="fw-bold text-dark">{serie.name}</h5>
                 <p className="text-muted small flex-grow-1" style={{ minHeight: "60px" }}>
                     {serie.description.length > 100
-                        ? serie.description.slice(0, 150) + "..."
+                        ? serie.description.slice(0, 100) + "..."
                         : serie.description}
                 </p>
                 <p className="text-secondary small">📚 {serie.number_volumes} volumi</p>
