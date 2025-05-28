bed8f03278f6d702f3f11d057a5a21d1872583aa
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-28 17:11:26 +0200

    style: add shadow to Navbar and wrap it in a header element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
                 <div className="container-fluid space ">
                     <a className="navbar-brand" href="#"><img src={logo} className="rounded-circle" style={{ width: "100px" }} alt="logo" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
