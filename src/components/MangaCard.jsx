906c1422bdc362097684f526d54d8cbebdea46f9
alemalagni <alemalagni@gmail.com>
2025-05-29 12:26:23 +0200

    style: fix price showing

--- src/components/MangaCard.jsx
+++ src/components/MangaCard.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom"
 
 function MangaCard({ data }) {
+    const prezzo = String(data.price);
+    let decimale = prezzo.slice(prezzo.indexOf(".") + 1);
+
+    if (prezzo.slice(prezzo.indexOf(".") + 1).length === 1) {
+        decimale = prezzo.slice(prezzo.indexOf(".") + 1) + "0";
+    } else if (prezzo.slice(prezzo.indexOf(".") + 1).length === 0) {
+        decimale = "00";
+    } else if (prezzo.slice(prezzo.indexOf(".") + 1).length === 2) {
+        decimale = prezzo.slice(prezzo.indexOf(".") + 1);
+    }
+
+    const prezzoNuovo = prezzo.slice(0, prezzo.indexOf(".")) + "," + decimale;
+
     return (
         <Link to={`/manga/${data.slug}`}>
             <div className="card shadow-sm h-100">
@@ -10,7 +23,7 @@ function MangaCard({ data }) {
                 <div className="card-body d-flex flex-column">
                     <div className="text-center">
                         <p><strong>{data.title}</strong></p>
-                        <p><strong>{`${data.price}€`}</strong></p>
+                        <p><strong>{`${prezzoNuovo}€`}</strong></p>
                         <p><strong>Genere:</strong> {`${data.genre}`}</p>
                     </div>
                     <div className="mt-auto">
