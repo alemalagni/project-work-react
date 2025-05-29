f1d60b1df32018a8b46f94045419e58530332bc6
severogianluca <gianlucasevero300@gmail.com>
2025-05-29 18:01:22 +0200

    fix color bf homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
     return (
 
         <>
-            <div className="gradient">
+            <div>
                 <HeroSpace />
                 <Carousel />
                 <Series />
